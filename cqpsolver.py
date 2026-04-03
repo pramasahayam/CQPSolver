Switched to basic scipy.spare implementation, fixed bug in max iteration logic, changed default tol to 1e-6, fixed shape bug with objective calculation

cqpsolver.py
@@ -1,33 +1,34 @@
 from dataclasses import dataclass
 
 import numpy as np
+import scipy.sparse as sp
 
 
 @dataclass(frozen=True)
 class Problem:
     """Convex quadratic program class to store problem parameters."""
 
-    Q: np.ndarray  # (n,n)
-    q: np.ndarray  # (n,1)
-    G: np.ndarray  # (p,n)
-    h: np.ndarray  # (p,1)
-    A: np.ndarray  # (m,n)
-    b: np.ndarray  # (m,1)
+    Q: sp.csc_array  # (n,n)
+    q: sp.csc_array  # (n,1)
+    G: sp.csc_array  # (p,n)
+    h: sp.csc_array  # (p,1)
+    A: sp.csc_array  # (m,n)
+    b: sp.csc_array  # (m,1)
 
     @property
     def n(self) -> int:
         """Dimension of q vector."""
-        return self.q.size
+        return self.Q.shape[0]
 
     @property
     def p(self) -> int:
         """Dimension of h vector."""
-        return self.h.size
+        return self.G.shape[0]
 
     @property
     def m(self) -> int:
         """Dimension of b vector."""
-        return self.b.size
+        return self.A.shape[0]
 
 @dataclass(frozen=True)
 class Residuals:
@@ -66,27 +67,28 @@ class Solver:
     def find_initial_state(self) -> SolverState:
         """Calculate the initial point (x0, s0, y0, z0)."""
         # Set up and solve linear system
-        LHS_row1: np.ndarray = np.block([self.prob.Q, self.prob.G.T, self.prob.A.T])
-        LHS_row2: np.ndarray = np.block([self.prob.G, -np.eye(self.prob.p), np.zeros([self.prob.p, self.prob.m])])
-        LHS_row3: np.ndarray = np.block(
+        LHS_row1: sp.csc_array = sp.hstack([self.prob.Q, self.prob.G.T, self.prob.A.T])
+        LHS_row2: sp.csc_array = sp.hstack(
+            [self.prob.G, -sp.eye(self.prob.p), sp.csc_array(np.zeros([self.prob.p, self.prob.m]))],
+        )
+        LHS_row3: sp.csc_array = sp.hstack(
             [
                 self.prob.A,
-                np.zeros([self.prob.m, self.prob.p]),
-                np.zeros([self.prob.m, self.prob.m]),
+                sp.csc_array(np.zeros([self.prob.m, self.prob.p])),
+                sp.csc_array(np.zeros([self.prob.m, self.prob.m])),
             ],
         )
-        LHS: np.ndarray = np.vstack([LHS_row1, LHS_row2, LHS_row3])
+        LHS: sp.csc_array = sp.vstack([LHS_row1, LHS_row2, LHS_row3], format="csc")
 
-        RHS: np.ndarray = np.vstack([-self.prob.q, self.prob.h, self.prob.b])
+        RHS: sp.csc_array = sp.vstack([-self.prob.q, self.prob.h, self.prob.b], format="csc")
 
-        sol: np.ndarray = np.linalg.solve(LHS, RHS)
+        sol: np.ndarray = sp.linalg.spsolve(LHS, RHS).reshape(-1, 1)
         x: np.ndarray = sol[: self.prob.n]
         y: np.ndarray = sol[-self.prob.m :] if self.prob.m > 0 else np.zeros((0, 1))
 
         # Set initial primal/dual variables
         x0: np.ndarray = x
         y0: np.ndarray = y
-
         z: np.ndarray = self.prob.G @ x - self.prob.h
         alpha_p: float = np.max(z)
         s0: np.ndarray = -z if alpha_p < 0 else -z + 1 + alpha_p
@@ -107,49 +109,49 @@ class Solver:
     def step(self, state: SolverState) -> SolverState:
         """Compute next iteration."""
         # Setup and solve KKT system for affine scaling directions
-        LHS_row1: np.ndarray = np.block(
+        LHS_row1: sp.csc_array = sp.hstack(
             [
                 self.prob.Q,
-                np.zeros([self.prob.n, self.prob.p]),
+                sp.csc_array(np.zeros([self.prob.n, self.prob.p])),
                 self.prob.G.T,
                 self.prob.A.T,
             ],
         )
-        LHS_row2: np.ndarray = np.block(
+        LHS_row2: sp.csc_array = sp.hstack(
             [
-                np.zeros([self.prob.p, self.prob.n]),
-                np.diag(state.z.flatten()),
-                np.diag(state.s.flatten()),
-                np.zeros([self.prob.p, self.prob.m]),
+                sp.csc_array(np.zeros([self.prob.p, self.prob.n])),
+                sp.diags_array(state.z.flatten()),
+                sp.diags_array(state.s.flatten()),
+                sp.csc_array(np.zeros([self.prob.p, self.prob.m])),
             ],
         )
-        LHS_row3: np.ndarray = np.block(
+        LHS_row3: sp.csc_array = sp.hstack(
             [
                 self.prob.G,
-                np.eye(self.prob.p),
-                np.zeros([self.prob.p, self.prob.p]),
-                np.zeros([self.prob.p, self.prob.m]),
+                sp.eye(self.prob.p),
+                sp.csc_array(np.zeros([self.prob.p, self.prob.p])),
+                sp.csc_array(np.zeros([self.prob.p, self.prob.m])),
             ],
         )
-        LHS_row4: np.ndarray = np.block(
+        LHS_row4: sp.csc_array = sp.hstack(
             [
                 self.prob.A,
-                np.zeros([self.prob.m, self.prob.p]),
-                np.zeros([self.prob.m, self.prob.p]),
-                np.zeros([self.prob.m, self.prob.m]),
+                sp.csc_array(np.zeros([self.prob.m, self.prob.p])),
+                sp.csc_array(np.zeros([self.prob.m, self.prob.p])),
+                sp.csc_array(np.zeros([self.prob.m, self.prob.m])),
             ],
         )
-        LHS: np.ndarray = np.vstack([LHS_row1, LHS_row2, LHS_row3, LHS_row4])
+        LHS: sp.csc_array = sp.vstack([LHS_row1, LHS_row2, LHS_row3, LHS_row4], format="csc")
 
-        RHS_aff_row1: np.ndarray = -(
-            self.prob.Q @ state.x + self.prob.q + self.prob.G.T @ state.z + self.prob.A.T @ state.y
+        RHS_aff_row1: sp.csc_array = -sp.csc_array(
+            self.prob.Q @ state.x + self.prob.q + self.prob.G.T @ state.z + self.prob.A.T @ state.y,
         )
-        RHS_aff_row2: np.ndarray = -state.s * state.z
-        RHS_aff_row3: np.ndarray = -(self.prob.G @ state.x + state.s - self.prob.h)
-        RHS_aff_row4: np.ndarray = -(self.prob.A @ state.x - self.prob.b)
-        RHS_aff: np.ndarray = np.vstack([RHS_aff_row1, RHS_aff_row2, RHS_aff_row3, RHS_aff_row4])
+        RHS_aff_row2: sp.csc_array = sp.csc_array(-state.s * state.z)
+        RHS_aff_row3: sp.csc_array = -sp.csc_array(self.prob.G @ state.x + state.s - self.prob.h)
+        RHS_aff_row4: sp.csc_array = -sp.csc_array(self.prob.A @ state.x - self.prob.b)
+        RHS_aff: sp.csc_array = sp.vstack([RHS_aff_row1, RHS_aff_row2, RHS_aff_row3, RHS_aff_row4], format="csc")
 
-        delta_aff: np.ndarray = np.linalg.solve(LHS, RHS_aff)
+        delta_aff: np.ndarray = sp.linalg.spsolve(LHS, RHS_aff).reshape(-1, 1)
         delta_s_aff: np.ndarray = delta_aff[self.prob.n : self.prob.n + self.prob.p]
         delta_z_aff: np.ndarray = delta_aff[self.prob.n + self.prob.p : self.prob.n + 2 * self.prob.p]
 
@@ -157,18 +159,17 @@ class Solver:
         mu: np.ndarray = state.s.T @ state.z / self.prob.p
 
         alpha: float = min(1, self.max_step(state.s, delta_s_aff), self.max_step(state.z, delta_z_aff))
-
         sigma: float = (
             (state.s + alpha * delta_s_aff).T @ (state.z + alpha * delta_z_aff) / (state.s.T @ state.z)
         ) ** 3
 
-        RHS_cc_row1: np.ndarray = np.zeros([self.prob.n, 1])
-        RHS_cc_row2: np.ndarray = sigma * mu - delta_s_aff * delta_z_aff
-        RHS_cc_row3: np.ndarray = np.zeros([self.prob.p, 1])
-        RHS_cc_row4: np.ndarray = np.zeros([self.prob.m, 1])
-        RHS_cc: np.ndarray = np.vstack([RHS_cc_row1, RHS_cc_row2, RHS_cc_row3, RHS_cc_row4])
+        RHS_cc_row1: sp.csc_array = sp.csc_array(np.zeros([self.prob.n, 1]))
+        RHS_cc_row2: sp.csc_array = sp.csc_array(sigma * mu - delta_s_aff * delta_z_aff)
+        RHS_cc_row3: sp.csc_array = sp.csc_array(np.zeros([self.prob.p, 1]))
+        RHS_cc_row4: sp.csc_array = sp.csc_array(np.zeros([self.prob.m, 1]))
+        RHS_cc: sp.csc_array = sp.vstack([RHS_cc_row1, RHS_cc_row2, RHS_cc_row3, RHS_cc_row4])
 
-        delta_cc: np.ndarray = np.linalg.solve(LHS, RHS_cc)
+        delta_cc: np.ndarray = (sp.linalg.spsolve(LHS, RHS_cc)).reshape(-1, 1)
 
         # Combine aff and cc directions
         delta: np.ndarray = delta_aff + delta_cc
@@ -187,7 +188,7 @@ class Solver:
         y_new: np.ndarray = state.y + step_size * delta_y
 
         # Find updated value of objective function
-        obj: float = 0.5 * (x_new.T @ self.prob.Q @ x_new) + self.prob.q.T @ x_new
+        obj: float = (0.5 * (x_new.T @ self.prob.Q @ x_new) + self.prob.q.T @ x_new).flatten()[0]
 
         # Find new Residuals
         new_res: Residuals = self.calc_residuals(x_new, s_new, z_new, y_new)
@@ -203,7 +204,7 @@ class Solver:
         """Solve problem from start to finish, returning state history."""
         state_history: list[SolverState] = [self.find_initial_state()]
 
-        while state_history[-1].iter <= self.max_iter:
+        while state_history[-1].iter < self.max_iter:
             if state_history[-1].residuals.converged(self.tol):
                 break
 